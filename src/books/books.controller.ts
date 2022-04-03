5bbfe40a4e16af80a70fb1a81bd21068ec88b33e
Arsen Mkrtychian <prostoprostor@gmail.com>
2022-04-03 16:12:47 +0300

    указаны типы, убраны лишние импорты

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -3,7 +3,6 @@ import { AddBookDto } from './dto/add-book.dto';
 import { RangeBookDto } from './dto/range-book.dto';
 import { BooksService } from './books.service';
 import { Book } from './models/book.model';
-import { IsInt } from 'sequelize-typescript';
 
 @Controller('api/books')
 export class BooksController {
@@ -31,8 +30,8 @@ export class BooksController {
         throw new HttpException('Параметры start и limit обязательны', 400);
       }
 
-      const start = Number(query.start);
-      const limit = Number(query.limit);
+      const start:number = Number(query.start);
+      const limit:number = Number(query.limit);
     
       if (!Number.isInteger(start) || !Number.isInteger(limit)) {
         throw new HttpException('Параметры должны быть целыми числами', 400);
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/sequelize';
 import { Book } from './models/book.model';
 import { AddBookDto } from './dto/add-book.dto';
 import { Op } from 'sequelize';
-import { RangeBookDto } from './dto/range-book.dto';
 
 @Injectable()
 export class BooksService {
@@ -15,9 +14,9 @@ export class BooksService {
     }
 
     //https://stackoverflow.com/questions/43115151
-    async showRange(start, limit): Promise<Book[]> {
-        const from = start;
-        const to = from + limit - 1;
+    async showRange(start:number, limit:number): Promise<Book[]> {
+        const from:number = start;
+        const to:number = from + limit - 1;
         
         return this.bookModel.findAll({
             where: {
