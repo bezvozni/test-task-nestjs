2b4455515828b6cc3ef8856b7c75a9456e78b052
Arsen Mkrtychian <prostoprostor@gmail.com>
2022-04-05 13:43:45 +0300

    первые юнит тесты котролера

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,18 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
 
 describe('BooksController', () => {
-  let controller: BooksController;
+  let booksController: BooksController;
+  
+  const mockBooksServise = {
+    showRange: jest.fn(dto => { return 'done'})
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
-    }).compile();
+      providers: [BooksService],
+    })
+      .overrideProvider(BooksService)
+      .useValue(mockBooksServise)
+      .compile();
 
-    controller = module.get<BooksController>(BooksController);
+    booksController = module.get<BooksController>(BooksController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(booksController).toBeDefined();
   });
+
+  it('should throw excepthion', () => {
+    expect(() => booksController.addBooks([
+      {name: 'test', author: 'definded', year: 0, read: true},
+      {name: 'name', author: undefined, year: undefined, read: undefined}
+    ])).toThrow('Поля name и author обязательны');
+  });
+
+  it('should call method and return its return', () => {
+    expect(booksController.showRange({start: 1, limit: 1})).toEqual('done');
+    expect(mockBooksServise.showRange).toHaveBeenCalled();
+  })
 });
